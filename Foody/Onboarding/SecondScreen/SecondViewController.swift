5ee093f185a802157a5c17e31b42fcb09c5ca45f
ezgikula <kula.ezgi@gmail.com>
2022-12-26 13:55:07 +0300

    Add imageView, Color Set, Label and Button

--- Foody/Onboarding/SecondScreen/SecondViewController.swift
+++ Foody/Onboarding/SecondScreen/SecondViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var startButtonTapped: UIButton!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstLabel.text = "No matter what I cook!"
+        firstLabel.font = UIFont(name: "Georgia",size: 25)
+        view.addSubview(firstLabel)
+        
+        secondLabel.text = "Delicious new recipes everyday.!"
+        secondLabel.font = UIFont(name: "Georgia",size: 18)
+        view.addSubview(secondLabel)
+        
+        startButtonTapped.layer.cornerRadius = 10.0
+        
+        imageView.image = UIImage(named: "second-onboarding")
+        
     }
-
-
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        print("BUTONA TIKLANDI.")
+        
+    }
 }
+
+
