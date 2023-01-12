2a74a59cadaaed7759551ce203eca7ed0c4b8f8a
mervenurgulbagci <mervenurgulbagci@gmail.com>
2023-01-13 01:15:32 +0300

    add login page

--- Foody/Onboarding/Login/LoginViewController.swift
+++ Foody/Onboarding/Login/LoginViewController.swift
@@ -28,49 +28,53 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        rememberMeSwitch.addTarget(self, action: #selector(self.stateChanged), for: .valueChanged)
-                let defaults: UserDefaults = UserDefaults.standard
-                if defaults.bool(forKey: "ISRemember") {
-                    emailTextFld.text = defaults.value(forKey: "SavedUserEmail") as! String
-                    passwordTextFld.text = defaults.value(forKey: "SavedPassword") as! String
-                    rememberMeSwitch.setOn(true, animated: false)
-                    print("already logged user")
-                }
-                else {
-                    rememberMeSwitch
-                        .setOn(false, animated: false)
-                }
-        
+        let currentUser = Auth.auth().currentUser
+        if currentUser != nil {
+            let defaults: UserDefaults = UserDefaults.standard
+            if defaults.bool(forKey: "ISRemember") {
+                rememberMeSwitch.setOn(true, animated: false)
+                print("already logged user")
+            }
+            else {
+                rememberMeSwitch.setOn(false, animated: false)
+            }
+        }
+        rememberMeSwitch.addTarget(self, action: #selector(self.rememberMeSwitchChck(_:)), for: .valueChanged)
+  
+        emailTextFld.layer.cornerRadius = 20
+        passwordTextFld.layer.cornerRadius = 20
         loginImageView.image = UIImage(named: "chicken")
         welcomeLbl.text = "Welcome"
         loginLbl.text =  "Login your account"
-        emailTextFld.layer.cornerRadius = 25
-        passwordTextFld.layer.cornerRadius = 25
         loginBttn.layer.cornerRadius = 15
         registerBttn.layer.cornerRadius = 15
     }
    
-    @objc func stateChanged(_ switchState: UISwitch) {
-           let defaults: UserDefaults? = UserDefaults.standard
-        if switchState.isOn {
-               defaults?.set(true, forKey: "ISRemember")
-               defaults?.set(emailTextFld.text, forKey: "SavedUserEmail")
-               defaults?.set(passwordTextFld.text, forKey: "SavedPassword")
-           }
-           else {
-               defaults?.set(false, forKey: "ISRemember")
-               }
-               }
-    @IBAction func rememberMeSwitchChck(_ sender: Any) {
-
+   
+    @IBAction func rememberMeSwitchChck(_ sender: UISwitch) {
+        let defaults: UserDefaults? = UserDefaults.standard
+        if sender.isOn {
+            defaults?.set(true, forKey: "ISRemember")
+            print("switch is on")
             }
+            else {
+                defaults?.set(false, forKey: "ISRemember")
+                print("switch is off")
+            }
+        }
+    
     @IBAction func loginBttnClicked(_ sender: Any) {
-        if emailTextFld.text != "" && passwordTextFld.text != "" {
-                   Auth.auth().signIn(withEmail: emailTextFld.text!, password: passwordTextFld.text!){
-                   (authdataresult, error) in
+        guard let email = emailTextFld.text else { return }
+        guard let password = passwordTextFld.text else { return }
+        if email != "" && password != "" {
+                Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
                        if error != nil{
-                           self.errorMessage(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Try again.")
+//                           self.errorMessage(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Try again.")
+                           if !email.isValidEmail(email: email){
+                               self.errorMessage(titleInput: "Error!", messageInput: "Please control the email")
+                           }else if !password.isValidPassword(password: password){
+                               self.errorMessage(titleInput: "Error!", messageInput: "Please control the password")
+                           }
                        }else{
                            self.errorMessage(titleInput: "WELCOME!", messageInput: error?.localizedDescription ?? "Success Login.")
                            UserDefaults.standard.set(true, forKey: "REMEMBER_USER")
@@ -78,37 +82,36 @@ class LoginViewController: UIViewController {
                        }
                    }
                }
-                   else{
+        else{
                    errorMessage(titleInput: "Error!", messageInput: "Enter the username and password")
                }
            }
+    
     func errorMessage(titleInput:String, messageInput:String){
            let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
            alert.addAction(okButton)
            self.present(alert, animated: true, completion: nil)
-           
        }
          
     @IBAction func clickedRegister(_ sender: Any) {
         clickedRegisterCompletion?()
     }
+    
 }
-//extension String {
-//
-//     func isValidEmail(email:String) -> Bool {
-//
-//         let emailRegex = "[a-zA-Z0-9.%-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,4}"
-//         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-//         let result = emailTest.evaluate(with: email)
-//         return result
-//     }
-//
-//     func isValidPassword(password:String) -> Bool {
-//
-//         let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,10}$"// Minimum 6 maximum 10 characters at least 1 Alphabet and 1 Number
-//         let passwordTest = NSPredicate(format:"SELF MATCHES %@", passwordRegex)
-//         let result = passwordTest.evaluate(with:password)
-//         return result
-//     }
-// }
+extension String {
+
+    func isValidEmail(email: String) -> Bool {
+         let emailRegex = "[a-zA-Z0-9.%-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,4}"
+         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+         let result = emailTest.evaluate(with: self)
+         return result
+     }
+
+    func isValidPassword(password: String) -> Bool {
+         let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,10}$"
+         let passwordTest = NSPredicate(format:"SELF MATCHES %@", passwordRegex)
+         let result = passwordTest.evaluate(with: self)
+         return result
+     }
+ }
