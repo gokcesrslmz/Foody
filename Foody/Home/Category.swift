7003cad1c6506eeba2010ff8d031b2e18bd579fe
ezgikula <kula.ezgi@gmail.com>
2023-03-22 12:38:22 +0300

    add collection view functions

---
+++ Foody/Home/Category.swift
@@ -0,0 +1,32 @@
+//
+//  Category.swift
+//  Foody
+//
+//  Created by Ezgi  on 30.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct Category{
+    let title:String
+    let image:UIImage
+}
+let categories:[Category] = [
+    
+Category(title: "Beef", image: UIImage(named: "chicken1")!),
+Category(title: "Breakfast", image: UIImage(named: "chicken1")!),
+Category(title: "Chicken", image:UIImage(named: "chicken1")! ),
+Category(title: "Dessert", image: UIImage(named: "chicken1")!),
+Category(title: "Goat", image: UIImage(named: "chicken1")!),
+Category(title: "Lamb", image: UIImage(named: "chicken1")!),
+Category(title: "Miscellaneous", image: UIImage(named: "chicken1")!),
+Category(title: "Pasta", image: UIImage(named: "chicken1")!),
+Category(title: "Pork", image: UIImage(named: "chicken1")!),
+Category(title: "Seafood", image: UIImage(named: "chicken1")!),
+Category(title: "Side", image: UIImage(named: "chicken1")!),
+Category(title: "Starter", image: UIImage(named: "chicken1")!),
+Category(title: "Vegan", image: UIImage(named: "chicken1")!),
+Category(title: "Vegatarian", image:UIImage(named: "chicken1")!)
+
+]
--- Podfile
+++ Podfile
@@ -5,6 +5,9 @@ target 'Foody' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
+pod 'ParseSwift'
+
+
 pod 'FirebaseAuth'
 pod ‘Firebase/Core’
 
