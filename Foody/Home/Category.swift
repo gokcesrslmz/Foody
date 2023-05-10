9e734cd2bc5a61648a51001ad1d85ba481cae1f6
gokcesarsilmaz <gokcesarsilmazz@gmail.com>
2023-05-10 18:16:15 +0200

    remove development team and adjust model

--- Foody/Home/Category.swift
+++ Foody/Home/Category.swift
@@ -8,13 +8,12 @@
 import Foundation
 import UIKit
 
-struct Category{
+struct Category {
     let title: String?
     let image: UIImage?
 }
 
 let categories: [Category] = [
-    
 Category(title: "Beef", image: UIImage(named: "chicken1")),
 Category(title: "Breakfast", image: UIImage(named: "chicken1")),
 Category(title: "Chicken", image:UIImage(named: "chicken1")),
@@ -29,5 +28,4 @@ Category(title: "Side", image: UIImage(named: "chicken1")),
 Category(title: "Starter", image: UIImage(named: "chicken1")),
 Category(title: "Vegan", image: UIImage(named: "chicken1")),
 Category(title: "Vegatarian", image:UIImage(named: "chicken1"))
-
 ]
